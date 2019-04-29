d9eb3a4b16f94c87803e0f4882b1e1a40fb347aa
Karan Gupta <gkaran184@gmail.com>
2019-04-29 13:06:48 +0530

    Added Preview Page and redirecting mechanism
    Signed-off-by: Karan Gupta <gkaran184@gmail.com>

--- transform/urls.py
+++ transform/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import upload
+from .views import upload, run_image, preview
 
 urlpatterns = [
     path('upload/', upload, name='upload'),
+    path('run/', run_image, name='run_image'),
+    path('preview/', preview, name='preview'),
 ]
--- transform/views.py
+++ transform/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import ImageModel
 from django.utils import timezone
+from django.http import JsonResponse
+import asyncio
 # Create your views here.
 
 def upload(request):
@@ -12,8 +14,22 @@ def upload(request):
 			imodel.image = request.FILES['image']
 			imodel.timestamp = timezone.datetime.now()
 			imodel.save()
-			return render(request, 'transform/generating.html')
+			return render(request, 'transform/generating.html', {'img': imodel})
 		else:
 			return render(request, 'transform/upload.html', {'error': 'Please provide a valid image'})
 		return render(request, 'transform/generating.html')
 
+
+def run_image(request):
+	if request.method == 'GET':
+		filename = request.headers['Filename']
+		cnn(filename)
+		return JsonResponse({'msg': 'success'})
+
+
+def cnn(filename):
+	print('Deep Learning ... on ' + str(filename))
+
+def preview(request):
+	filename = request.GET['filename']
+	return render(request, 'transform/preview.html', {'filename': filename})
